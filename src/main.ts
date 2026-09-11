a370c6b0eaca471e04f909cb961a8040bed54342
ttetrafon <ttetrafon@yahoo.gr>
2026-09-11 15:55:45 +0300

    fixes && logger test

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,10 @@
+import { AppName } from './util/constants';
+// components
+// styles
 import './styles/style.css';
+// services
+import { Logger } from '../library/services/logger';
+
+const logger: Logger = new Logger('debug', AppName);
+
+logger.log("Test?!?!?");
